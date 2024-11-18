a739cdee05bed4708f68eb500b1e04534106be4d
Mohamed Al Mehairbi <62325490+ItIsRain@users.noreply.github.com>
2024-11-18 17:30:27 +0400

    Update vite.config.ts

--- adeo-request-system-frontend/vite.config.ts
+++ adeo-request-system-frontend/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -15,6 +18,14 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
     },
   },
 })
